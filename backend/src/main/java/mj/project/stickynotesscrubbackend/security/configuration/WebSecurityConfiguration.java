000af35cff9feed6db6fd0cb5f348238998a32b5
Michal <michaljanuszkiewicz6@gmail.com>
2022-05-02 15:11:02 +0200

    Fix CORS configuration

--- backend/src/main/java/mj/project/stickynotesscrubbackend/security/configuration/WebSecurityConfiguration.java
+++ backend/src/main/java/mj/project/stickynotesscrubbackend/security/configuration/WebSecurityConfiguration.java
@@ -3,6 +3,7 @@ package mj.project.stickynotesscrubbackend.security.configuration;
 import mj.project.stickynotesscrubbackend.security.AuthTokenFilter;
 import mj.project.stickynotesscrubbackend.security.jwt.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,11 +20,16 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -33,8 +39,11 @@ import java.io.IOException;
         prePostEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
     private final UserDetailsService appUserService;
     private final AuthTokenFilter authenticationJwtTokenFilter;
+
 
     @Autowired
     public WebSecurityConfiguration(UserDetailsService appUserService, AuthTokenFilter authenticationJwtTokenFilter) {
@@ -45,13 +54,24 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors().and()
+                .cors().configurationSource(request -> corsConfigurationSource()).and()
                 .csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint()).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/v1/auth/**").permitAll()
                 .anyRequest().authenticated();
         http.addFilterBefore(authenticationJwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public CorsConfiguration corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        corsConfiguration.setAllowedOriginPatterns(List.of(frontendUrl));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setExposedHeaders(List.of("Authorization"));
+        return corsConfiguration;
     }
 
     @Override
